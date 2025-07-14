e680b05c0d9a914de05bb98d8cc98791a94e80b8
ahmadrifai <ahmatrifai8721@gmail.com>
2025-07-14 12:45:32 +0700

    update view siswa Show pj pkl 2

--- resources/views/admin/siswa/siswaShow.blade.php
+++ resources/views/admin/siswa/siswaShow.blade.php
@@ -167,7 +167,7 @@
                                     <div class="form-floating mb-3">
                                         <input type="text" readonly class="form-control disabled" id="floatingKelas"
                                             value="{{ $siswa->mapingPKL->first()->tempat->penanggungJawab->nama ?? 'Temapat PKL Belum Di isi' }}" />
-                                        <label for="floatingKelas">Temapat PKL</label>
+                                        <label for="floatingKelas">Penggung Jawab Temapat PKL</label>
                                     </div>
                                 </div>
                             </div>
